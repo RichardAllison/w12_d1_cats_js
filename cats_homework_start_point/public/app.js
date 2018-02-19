99192a84046f930d3c110ee7e9bc650a4a58281e
Richard Allison <richard.allison.18@outlook.com>
2018-02-19 20:56:38 +0000

    Removed the cat data from the HTML and added it via the javascript code

--- cats_homework_start_point/public/app.js
+++ cats_homework_start_point/public/app.js
@@ -1,3 +1,10 @@
+const CATS_ARRAY = [
+  { name: 'Boba', favouriteFood: 'Sock fluff', url: "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"},
+  { name: 'Barnaby', favouriteFood: 'Tuna', url: "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"},
+  { name: 'Max', favouriteFood: 'Whiskas Temptations', url: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"},
+  { name: 'Buster', favouriteFood: 'Cat food', url: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/800px-Cat03.jpg"}
+]
+
 const createUnorderedList = function(){
   const unorderedList = document.createElement('ul');
   unorderedList.classList.add('cat');
@@ -40,7 +47,9 @@ const addCat = function(name, favouriteFood, url) {
 }
 
 const app = function() {
-  addCat('Buster', 'Cat food', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/800px-Cat03.jpg')
-};
+  for (const cat of CATS_ARRAY) {
+    addCat(cat.name, cat.favouriteFood, cat.url);
+  }
+}
 
 window.addEventListener("DOMContentLoaded", app);
